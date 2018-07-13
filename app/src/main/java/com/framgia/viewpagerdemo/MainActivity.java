3a9e5ef73d8f471dda9c8c8d5c7db0e86fd551e5
Hades <Hphamptit@gmail.com>
2018-07-13 09:06:51 +0700

    Update Screenshot

--- app/src/main/java/com/framgia/viewpagerdemo/MainActivity.java
+++ app/src/main/java/com/framgia/viewpagerdemo/MainActivity.java
@@ -1,13 +1,34 @@
 package com.framgia.viewpagerdemo;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ViewPager mPager;
+    private TabLayout mTabLayout;
+    private SimplePagerAdapter mAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mPager = findViewById(R.id.viewPager);
+        mTabLayout = findViewById(R.id.tabLayout);
+        mAdapter = new SimplePagerAdapter(this, getSupportFragmentManager());
+
+        mPager.setAdapter(mAdapter);
+        mTabLayout.setupWithViewPager(mPager);
+        setupTabIcon();
+
+    }
+
+    private void setupTabIcon() {
+        for (int i = 0; i < mTabLayout.getTabCount(); i++) {
+            mTabLayout.getTabAt(i).setIcon(R.mipmap.ic_launcher);
+        }
     }
 }
